2407919b8163a5e743a91b779fff06d8f000cfa0
Haseebm01 <haseebdr01@gmail.com>
2025-09-22 20:22:55 -0700

    modified

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,44 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import { CommonEngine, isMainModule } from '@angular/ssr/node';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import bootstrap from './main.server';
 
-import bootstrap from '../src/main.server';
-
-const server = express();
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
 
+const app = express();
 const commonEngine = new CommonEngine();
 
-server.set('view engine', 'html');
-server.set('views', browserDistFolder);
+/**
+ * Example Express Rest API endpoints can be defined here.
+ * Uncomment and define endpoints as necessary.
+ *
+ * Example:
+ * ```ts
+ * app.get('/api/**', (req, res) => {
+ *   // Handle API request
+ * });
+ * ```
+ */
 
-// Serve static files from /browser
-server.get('*.*', express.static(browserDistFolder, {
-  maxAge: '1y'
-}));
+/**
+ * Serve static files from /browser
+ */
+app.get(
+  '**',
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: 'index.html'
+  }),
+);
 
-// All regular routes use the Angular engine
-server.get('*', (req, res, next) => {
+/**
+ * Handle all other requests by rendering the Angular application.
+ */
+app.get('**', (req, res, next) => {
   const { protocol, originalUrl, baseUrl, headers } = req;
 
   commonEngine
@@ -37,8 +53,15 @@ server.get('*', (req, res, next) => {
     .catch((err) => next(err));
 });
 
-const port = process.env['PORT'] || 4000;
+/**
+ * Start the server if this module is the main entry point.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ */
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
 
-server.listen(port, () => {
-  console.log(`Node Express server listening on http://localhost:${port}`);
-});
+export default app;
