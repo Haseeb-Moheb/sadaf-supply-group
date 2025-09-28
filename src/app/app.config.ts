ec607576e80c4ae9d29577f9f091bd036b124d6a
Haseebm01 <haseebdr01@gmail.com>
2025-09-28 12:32:57 -0700

    modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { provideServerRendering } from '@angular/platform-server';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ApplicationRef } from '@angular/core';
 
-export default function render(context: any) {
+export default function bootstrap(context: any): Promise<ApplicationRef> {
   return bootstrapApplication(AppComponent, {
     ...config,
     providers: [
